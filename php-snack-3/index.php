3e2d4bab63d9159aa19a73fd3b4629bd20714b9a
fedPass <federica.passanante@gmail.com>
2020-01-22 18:06:59 +0100

    es 3

--- php-snack-3/index.php
+++ php-snack-3/index.php
@@ -1,2 +1,24 @@
 <!-- Creare una funzione che restituisce un array con 15 numeri casuali, tenendo
 conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php
+
+$numbers = random_number(1, 100, 15);
+foreach ($numbers as $value) {
+    echo $value . ' ';
+};
+
+function random_number($min, $max, $items_number) {
+    //creo un array contenitore
+    $list_numbers = [];
+    //controllare che il nuovo array non superi la lunghezza stabilita
+    while (count($list_numbers) < $items_number) {
+        //genera numero random usando rand()
+        $number = rand($min, $max);
+        //se non è già in array pushalo
+        if (!in_array($number,$list_numbers)) {
+            $list_numbers[] = $number;
+        }
+    }
+    return $list_numbers;
+}
+ ?>
