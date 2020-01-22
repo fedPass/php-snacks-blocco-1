0523d10f0d3c0c2dee5545b705d908b038f9da3b
fedPass <federica.passanante@gmail.com>
2020-01-22 17:20:58 +0100

    es 1

--- php-snack-1/index.php
+++ php-snack-1/index.php
@@ -6,9 +6,39 @@ metodi che non conosciamo nella documentazione) che:
 Se tutto è ok, stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-
-foreach ($_GET as $key => $value) {
-    echo  $key . ' : ' . $value . '<br>';
+//recupero il parametro Nome
+$name = $_GET['name'];
+//estraggo al sua lunghezza
+$name_lenght = mb_strlen($name);
+if ($name_lenght < 3) {
+    echo 'il nome deve contenere almeno 3 lettere..accesso negato';
+} else {
+    //se il nome è corretto procede con la verifica della mail
+    //recupero mail
+    $mail = $_GET['mail'];
+    //verifico se è presente il punto
+    //se manca il punto errore
+    if (strpos($mail,'.') === false) {
+        echo 'manca il . nella mail...accesso negato';
+    } else {
+        //se il punto c'è controlla la @
+        //se la @ manca errore
+        if (strpos($mail,'@') === false) {
+            echo 'manca la @ nella mail...accesso negato';
+        } else {
+            //se ci sono entrambi passa a controllare l'età
+            //recupero eta
+            $eta = $_GET['age'];
+            //verifico se è un numero
+            //se non è numero Error
+            if (!is_numeric($eta)) {
+                echo 'non hai inserito un numero per gli anni...accesso negato';
+            } else {
+                //se è un Number
+                echo 'ACCESSO RIUSCITO';
+            }
+        }
+    }
 }
 
  ?>
