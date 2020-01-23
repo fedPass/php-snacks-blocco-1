1252171681421bf02ad32b0ca0963f480341a4a3
fedPass <federica.passanante@gmail.com>
2020-01-23 12:38:37 +0100

    correzione es 5

--- php-snack-5/index.php
+++ php-snack-5/index.php
@@ -1,3 +1,44 @@
 <!-- Utilizzare questo array: https://pastebin.com/CkX3680A.
 Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i
 PM in un rettangolo verde. -->
+
+<?php
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+?>
+
+<?php
+foreach ($db as $ruole => $persons) {
+    if($ruole == 'teachers') { ?>
+        <div style="background-color:grey" class="teacher">
+            <h2>Teachers</h2>
+    <?php } else { ?>
+        <div style="background-color:green" class="pm">
+            <h2>PM</h2>
+            <?php }
+                foreach ($persons as $person) {
+                echo "Nome: ".$person['name'].', Cognome:'.$person['lastname'].'<br>'; ?>
+            <?php } ?>
+        </div>
+<?php } ?>
